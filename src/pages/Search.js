2dcc68bec979b91a06e0bf4a17c2f29a6917f682
insideplant <kami.10pearl@gmail.com>
2021-11-03 10:15:35 +0800

    make layout,header-component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Search = () => {
   return (
     <div>
-      Search page
+      <Layout>      
+        Search page
+      </Layout>
     </div>
   )
 }
--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Top = () => {
   return (
     <div>
-      Top page
+      <Layout>
+        Top page
+      </ Layout>
     </div>
   )
 }
